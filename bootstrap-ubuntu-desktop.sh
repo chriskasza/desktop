c11d94daa977f799ee5165054872ca7af9b31bfa
Chris Kasza <chris.kasza@gmail.com>
2019-12-10 14:06:54 -0400

    Install docker with apt

--- bootstrap-ubuntu-desktop.sh
+++ bootstrap-ubuntu-desktop.sh
@@ -1,10 +1,39 @@
 #!/bin/sh
 set -ex
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y zsh vim tmux gnome-tweak-tool
-sudo apt install -y fonts-powerline powerline
-sudo snap install chromium chromium-ffmpeg docker
+sudo apt update
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common
+sudo apt upgrade -y
+sudo apt install -y \
+    fonts-powerline \
+    gnome-tweak-tool \
+    gnupg-agent \
+    powerline \
+    tmux \
+    vim \
+    zsh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   disco \
+   stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo curl -L \
+    "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo snap install chromium chromium-ffmpeg
 sudo snap install --classic code slack
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
